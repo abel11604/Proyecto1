c18c65abcef5fdcca2e763ec287411d41a5bddcd
abel11604 <abelcorona.quintero@gmail.com>
2024-02-14 23:34:36 -0700

    Update TransaccionDAO.java

--- proyectoBanco/src/main/java/dao/TransaccionDAO.java
+++ proyectoBanco/src/main/java/dao/TransaccionDAO.java
@@ -31,20 +31,18 @@ public class TransaccionDAO implements ITransaccion {
     @Override
     public Transaccion registrarTransaccion(Transaccion t) {
 
-        String creaTransaccion = "INSERT INTO transaccion "
-                + "(fecha_transaccion, hora_transaccion, cantidad, es_cliente, id_cuenta)"
-                + "VALUES (?,?,?,?,?)";
+        String creaTransaccion = "INSERT INTO Transaccion "
+                + "(cantidad, tipo_transaccion, id_cuenta)"
+                + "VALUES (?, ?, ?)";
 
         Transaccion tr = null;
 
         try {
             Connection con = conexion.crearConexion();
             PreparedStatement st = con.prepareStatement(creaTransaccion);
-            st.setDate(1, t.getFecha_transaccion());
-            st.setTime(2, t.getHora_transaccion());
-            st.setInt(3, t.getCantidad());
-            st.setBoolean(4, t.isTipo_transaccion());
-            st.setInt(5, t.getCuenta().getIdCuenta());
+            st.setInt(1, t.getCantidad());
+            st.setBoolean(2, t.isTipo_transaccion());
+            st.setInt(3, t.getCuenta().getIdCuenta());
 
         } catch (SQLException e) {
             Logger.getLogger(TransaccionDAO.class.getName()).log(Level.SEVERE, "Error al intentar eliminar la cuenta", e);
@@ -55,8 +53,8 @@ public class TransaccionDAO implements ITransaccion {
 
     @Override
     public Transaccion editarTransaccion(Transaccion t) {
-      Logger.getLogger(TransaccionDAO.class.getName()).log(Level.SEVERE, "No puedes editar transacciones ...");
-      return null;
+        Logger.getLogger(TransaccionDAO.class.getName()).log(Level.SEVERE, "No puedes editar transacciones ...");
+        return null;
 
     }
 
@@ -73,8 +71,7 @@ public class TransaccionDAO implements ITransaccion {
                 int filasAfectadas = deleteStatement.executeUpdate();
                 if (filasAfectadas > 0) {
                     transaccionEliminada.setId_transaccion(t.getId_transaccion());
-                    transaccionEliminada.setFecha_transaccion(t.getFecha_transaccion());
-                    transaccionEliminada.setHora_transaccion(t.getHora_transaccion());
+                    transaccionEliminada.setFechahora_transaccion(t.getFechahora_transaccion());
                     transaccionEliminada.setCantidad(t.getCantidad());
                     transaccionEliminada.setTipo_transaccion(t.isTipo_transaccion());
                     transaccionEliminada.setCuenta(t.getCuenta());
@@ -91,32 +88,29 @@ public class TransaccionDAO implements ITransaccion {
 
     @Override
     public Transaccion verTransaccion(int id) {
-        
-        
-        String selectTransaccion
-                = "SELECT t.id_transaccion,t.fecha_transaccion,t.hora_transaccion,"
-                + "t.cantidad,t.tipo_transaccion, c.id_cuenta"
-                + "FROM transaccion t "
-                + "JOIN cuenta c ON t.id_cuenta = c.id_cuenta "
-                + "WHERE t.id_transaccion = ?";
-        Transaccion transaccionEncontrada = new Transaccion();
+
+       String selectTransaccion
+        = "SELECT t.id_transaccion, t.fecha_transaccion, t.hora_transaccion, "
+        + "t.cantidad, t.tipo_transaccion, c.id_cuenta "
+        + "FROM transaccion t "
+        + "JOIN cuenta c ON t.id_cuenta = c.id_cuenta "
+        + "WHERE t.id_transaccion = ?";
+        Transaccion transaccionEncontrada = null;
 
         try {
             Connection c = conexion.crearConexion();
             PreparedStatement selectStatement = c.prepareStatement(selectTransaccion);
 
             selectStatement.setInt(1, id);
-
             ResultSet resultSet = selectStatement.executeQuery();
 
             if (resultSet.next()) {
                 transaccionEncontrada = new Transaccion();
                 transaccionEncontrada.setId_transaccion(resultSet.getInt("id_transaccion"));
-                transaccionEncontrada.setFecha_transaccion(resultSet.getDate("fecha_transaccion"));
-                transaccionEncontrada.setHora_transaccion(resultSet.getTime("hora_transaccion"));
+                transaccionEncontrada.setFechahora_transaccion(resultSet.getTimestamp("fecha_hora_transaccion"));
                 transaccionEncontrada.setTipo_transaccion(resultSet.getBoolean("tipo_transaccion"));
                 transaccionEncontrada.setCantidad(resultSet.getInt("cantidad"));
-                
+
                 Cuenta cuenta = new Cuenta();
                 cuenta.setIdCuenta(resultSet.getInt("id_cuenta"));
 
@@ -130,10 +124,5 @@ public class TransaccionDAO implements ITransaccion {
         }
         return transaccionEncontrada;
     }
-        
 
-
-
-    }
-
-
+}
